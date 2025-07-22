ddd99a953b269e63cdf031650649cc72427906cc
Sankaranandh TN <86720185+sankartn@users.noreply.github.com>
2025-07-22 15:49:50 +0530

    test

--- api/database.ts
+++ api/database.ts
@@ -4,12 +4,26 @@ const chalk = require('chalk');
 const connectDB: Function = async () => {
   try {
     // MongoDB setup.
-    mongoose.set('useFindAndModify', false);
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useNewUrlParser', true);
-    mongoose.set('useUnifiedTopology', true);
+    // mongoose.set('useFindAndModify', false);
+    // mongoose.set('useCreateIndex', true);
+    // mongoose.set('useNewUrlParser', true);
+    // mongoose.set('useUnifiedTopology', true);
     console.log("process.env");
     console.log(process.env);
+
+    const dns = require('node:dns');
+    const os = require('node:os');
+
+    // const options = { family: 4 };
+
+    dns.lookup(os.hostname()/*, options*/, (err, addr) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`IP address: ${addr}`);
+      }
+    });
+
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Mongoose connected");
   } catch (e) {

fb37d2ef97c813d6dcd2713ee27466af23376372
Sankaranandh TN <86720185+sankartn@users.noreply.github.com>
2025-07-24 10:28:48 +0530

    test

--- api/database.ts
+++ api/database.ts
@@ -10,20 +10,6 @@ const connectDB: Function = async () => {
     // mongoose.set('useUnifiedTopology', true);
     console.log("process.env");
     console.log(process.env);
-
-    const dns = require('node:dns');
-    const os = require('node:os');
-
-    // const options = { family: 4 };
-
-    dns.lookup(os.hostname()/*, options*/, (err, addr) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`IP address: ${addr}`);
-      }
-    });
-
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("Mongoose connected");
   } catch (e) {

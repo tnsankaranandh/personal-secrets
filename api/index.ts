52a92ebbfe4e1d98b823755b34f00eb1069103ae
Sankaranandh TN <86720185+sankartn@users.noreply.github.com>
2025-08-12 11:24:48 +0530

    test

--- api/index.ts
+++ api/index.ts
@@ -16,16 +16,6 @@ app.use(express.static('public'));
 
 app.get('/login', (req, res) => res.sendFile(getUIPageWithPath('login')));
 
-app.post('/uploadSuccessful', urlencodedParser, async (req, res) => {
-	try {
-		await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
-		res.status(200).send('<h1>User added successfully</h1>');
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Error adding user');
-	}
-});
-
 app.get('/allUsers', async (req, res) => {
 	try {
 		const users = await sql`SELECT * FROM Users;`;

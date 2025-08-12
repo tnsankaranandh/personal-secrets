f591b0bb37a8b33bf1b58e6d830e9f9258ab3a39
Sankaranandh TN <86720185+sankartn@users.noreply.github.com>
2025-08-12 11:30:04 +0530

    test

--- api/index.ts
+++ api/index.ts
@@ -16,78 +16,78 @@ app.use(express.static('public'));
 
 // app.get('/login', (req, res) => res.sendFile(getUIPageWithPath('login')));
 
-app.get('/allUsers', async (req, res) => {
-	try {
-		const users = await sql`SELECT * FROM Users;`;
-		if (users && users.rows.length > 0) {
-			let tableContent = users.rows
-				.map(
-					(user) =>
-						`<tr>
-                        <td>${user.id}</td>
-                        <td>${user.name}</td>
-                        <td>${user.email}</td>
-                    </tr>`
-				)
-				.join('');
+// app.get('/allUsers', async (req, res) => {
+// 	try {
+// 		const users = await sql`SELECT * FROM Users;`;
+// 		if (users && users.rows.length > 0) {
+// 			let tableContent = users.rows
+// 				.map(
+// 					(user) =>
+// 						`<tr>
+//                         <td>${user.id}</td>
+//                         <td>${user.name}</td>
+//                         <td>${user.email}</td>
+//                     </tr>`
+// 				)
+// 				.join('');
 
-			res.status(200).send(`
-                <html>
-                    <head>
-                        <title>Users</title>
-                        <style>
-                            body {
-                                font-family: Arial, sans-serif;
-                            }
-                            table {
-                                width: 100%;
-                                border-collapse: collapse;
-                                margin-bottom: 15px;
-                            }
-                            th, td {
-                                border: 1px solid #ddd;
-                                padding: 8px;
-                                text-align: left;
-                            }
-                            th {
-                                background-color: #f2f2f2;
-                            }
-                            a {
-                                text-decoration: none;
-                                color: #0a16f7;
-                                margin: 15px;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <h1>Users</h1>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>User ID</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                ${tableContent}
-                            </tbody>
-                        </table>
-                        <div>
-                            <a href="/">Home</a>
-                            <a href="/uploadUser">Add User</a>
-                        </div>
-                    </body>
-                </html>
-            `);
-		} else {
-			res.status(404).send('Users not found');
-		}
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Error retrieving users');
-	}
-});
+// 			res.status(200).send(`
+//                 <html>
+//                     <head>
+//                         <title>Users</title>
+//                         <style>
+//                             body {
+//                                 font-family: Arial, sans-serif;
+//                             }
+//                             table {
+//                                 width: 100%;
+//                                 border-collapse: collapse;
+//                                 margin-bottom: 15px;
+//                             }
+//                             th, td {
+//                                 border: 1px solid #ddd;
+//                                 padding: 8px;
+//                                 text-align: left;
+//                             }
+//                             th {
+//                                 background-color: #f2f2f2;
+//                             }
+//                             a {
+//                                 text-decoration: none;
+//                                 color: #0a16f7;
+//                                 margin: 15px;
+//                             }
+//                         </style>
+//                     </head>
+//                     <body>
+//                         <h1>Users</h1>
+//                         <table>
+//                             <thead>
+//                                 <tr>
+//                                     <th>User ID</th>
+//                                     <th>Name</th>
+//                                     <th>Email</th>
+//                                 </tr>
+//                             </thead>
+//                             <tbody>
+//                                 ${tableContent}
+//                             </tbody>
+//                         </table>
+//                         <div>
+//                             <a href="/">Home</a>
+//                             <a href="/uploadUser">Add User</a>
+//                         </div>
+//                     </body>
+//                 </html>
+//             `);
+// 		} else {
+// 			res.status(404).send('Users not found');
+// 		}
+// 	} catch (error) {
+// 		console.error(error);
+// 		res.status(500).send('Error retrieving users');
+// 	}
+// });
 
 // app.all('*', (req: any, res: any) => res.redirect('/login'));
 
